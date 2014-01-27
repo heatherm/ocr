b9c95a59d187cb98a08dc345ad4bb12b813d15e7
Heather Moore <moore.hj@gmail.com>
2014-01-27 11:45:04 -0800

    introduce all numbers as matrices

--- src/numbers.rb
+++ src/numbers.rb
@@ -1,3 +1,12 @@
 module Numbers
   ZERO = [[nil,0,nil], [1,nil,1], [1,0,1], [nil,nil,nil]]
+  ONE = [[nil,nil,nil], [nil,nil,1], [nil,nil,1], [nil,nil,nil]]
+  TWO = [[nil,0,nil],[nil,0,1],[1,0,nil],[nil,nil,nil]]
+  THREE = [[nil,0,nil],[nil,0,1],[nil,0,1],[nil,nil,nil]]
+  FOUR = [[nil,nil,nil],[1,0,1],[nil,nil,1],[nil,nil,nil]]
+  FIVE = [[nil,0,nil],[1,0,nil],[nil,0,1],[nil,nil,nil]]
+  SIX = [[nil,0,nil],[1,0,nil],[1,0,1],[nil,nil,nil]]
+  SEVEN = [[nil,0,nil],[nil,nil,1],[nil,nil,1],[nil,nil,nil]]
+  EIGHT = [[0,nil,0],[1,0,1],[1,0,1],[nil,nil,nil]]
+  NINE = [[nil,0,nil],[1,0,1],[nil,0,1],[nil,nil,nil]]
 end
