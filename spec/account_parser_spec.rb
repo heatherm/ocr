2c033d8c32bbebe018b5dfd6eb5d0bf3576556bc
Heather Moore <moore.hj@gmail.com>
2014-01-27 11:45:21 -0800

    ensure parser can deal with multiple entries

--- spec/account_parser_spec.rb
+++ spec/account_parser_spec.rb
@@ -11,5 +11,13 @@ describe AccountParser do
       expect(entry[0][n]).to eql(Numbers::ZERO)
     end
   end
-  
+
+  it 'parses multiple account numbers' do
+    all_zeros = " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n"
+    all_ones = "                           \n  |  |  |  |  |  |  |  |  |\n  |  |  |  |  |  |  |  |  |\n                           \n"
+    entry = AccountParser.parse(all_zeros+all_ones)
+    (0..8).step(1) do |n|
+      expect(entry[1][n]).to eql(Numbers::ONE)
+    end
+  end
 end
