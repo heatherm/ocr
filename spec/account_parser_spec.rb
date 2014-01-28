8173d78c0aca2fece6d7ede6d01088472b738a85
Heather Moore <moore.hj@gmail.com>
2014-01-27 17:34:59 -0800

    use stringio instead of string in specs to match
    reality

--- spec/account_parser_spec.rb
+++ spec/account_parser_spec.rb
@@ -5,7 +5,8 @@ describe AccountParser do
   include Numbers
 
   it 'parses a single account number' do
-    all_zeros = " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n"
+    all_zeros = StringIO.new(" _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n")
+
     entry = AccountParser.parse(all_zeros)
     (0..8).step(1) do |n|
       expect(entry[0][n]).to eql(Numbers::ZERO)
@@ -15,7 +16,7 @@ describe AccountParser do
   it 'parses multiple account numbers' do
     all_zeros = " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n"
     all_ones = "                           \n  |  |  |  |  |  |  |  |  |\n  |  |  |  |  |  |  |  |  |\n                           \n"
-    entry = AccountParser.parse(all_zeros+all_ones)
+    entry = AccountParser.parse(StringIO.new(all_zeros+all_ones))
     (0..8).step(1) do |n|
       expect(entry[1][n]).to eql(Numbers::ONE)
     end
--- src/account_parser.rb
+++ src/account_parser.rb
@@ -3,7 +3,7 @@ require_relative 'raw_account_number'
 class AccountParser
   def self.parse input
     all_entries = []
-    input.split(/(\n)/).delete_if{ |e| e == "\n" }.each_slice(4) do |four_lines|
+    input.readlines.delete_if{ |e| e == "\n" }.each_slice(4) do |four_lines|
       all_entries << RawAccountNumber.new(four_lines).as_numbers
     end
     all_entries
