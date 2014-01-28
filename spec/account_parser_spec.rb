0773dd63ee7c4c1bbe0694ba42585cd999ae8556
Heather Moore <moore.hj@gmail.com>
2014-01-28 08:01:43 -0800

    account parser can handle multiple entries

--- spec/account_parser_spec.rb
+++ spec/account_parser_spec.rb
@@ -4,21 +4,17 @@ require_relative '../lib/numbers'
 describe AccountParser do
   include Numbers
 
-  it 'parses a single account number' do
-    all_zeros = StringIO.new(" _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n")
-
-    entry = AccountParser.parse(all_zeros)
-    (0..8).step(1) do |n|
-      expect(entry[0][n]).to eql(Numbers::ZERO)
-    end
-  end
-
   it 'parses multiple account numbers' do
     all_zeros = " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           \n"
     all_ones = "                           \n  |  |  |  |  |  |  |  |  |\n  |  |  |  |  |  |  |  |  |\n                           \n"
     entry = AccountParser.parse(StringIO.new(all_zeros+all_ones))
     (0..8).step(1) do |n|
-      expect(entry[1][n]).to eql(Numbers::ONE)
+      actual = String(entry[0][n])
+      expect(actual).to eql("0")
+    end
+
+    (0..8).step(1) do |n|
+      expect(entry[1][n]).to eql('1')
     end
   end
 end
--- src/account_parser.rb
+++ src/account_parser.rb
@@ -1,11 +1,15 @@
 require_relative 'raw_account_number'
+require_relative 'number_matrix_converter'
+require_relative '../lib/numbers'
 
 class AccountParser
+  include Numbers
+
   def self.parse input
     all_entries = []
     input.readlines.delete_if{ |e| e == "\n" }.each_slice(4) do |four_lines|
       all_entries << RawAccountNumber.new(four_lines).as_numbers
     end
-    all_entries
+    NumberMatrixConverter.new(all_entries).convert
   end
 end
